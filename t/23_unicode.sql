00a0edbd9be7f198eeefd42fd6ee9b7d2f6c236c
Adam Kennedy <adamk@cpan.org>
2012-02-23 02:08:40 +0000

    Import of ADAMK/ORLite-1.91 from CPAN.
    gitpan-cpan-distribution: ORLite
    gitpan-cpan-version:      1.91
    gitpan-cpan-path:         ADAMK/ORLite-1.91.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- t/23_unicode.sql
+++ t/23_unicode.sql
@@ -1,6 +1,9 @@
 create table foo (
-    id text primary key,
-    text text
+    foo_id integer not null primary key,
+    one integer not null,
+    two real not null,
+    name text not null unique,
+    text text not null
 );
 
-insert into foo values ('smiley', '☺');
+insert into foo (one, two, name, text) values ( 1, 1.23, 'smiley', '☺');
